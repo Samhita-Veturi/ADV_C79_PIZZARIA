8686511e3dda92eef95966c9c7de315c418e8997
Samhita-Veturi <67774716+Samhita-Veturi@users.noreply.github.com>
2020-12-19 14:58:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+Menu_List = [
+    "Chicken Tandoori Pizza",
+    "Tandoori Paneer",
+    "Paneer Tikka Pizza",
+    "Deluxe Veggie Pizza",
+    "Veg Extravaganza Pizza",
+    "Peppy Paneer",
+    "Veggie Delight",
+    "Veg Supreme"
+];
+
+function Get_Menu(){
+    var htmlData;
+    htmlData="<ol class='menulist'>"
+    Menu_List.sort()
+    for(var i=0;i<Menu_List.length;i++){
+        htmlData = htmlData+'<li>' + Menu_List[i] +'</li>'
+    }
+    htmlData= htmlData+"</ol>"
+    document.getElementById("Display-Menu").innerHTML = htmlData;
+
+}
+
+function Add_Item(){
+    var htmlDATA;
+    var item = document.getElementById("add_item").value;
+    Menu_List.push(item);
+    Menu_List.sort;
+    htmlDATA = "<section class='cards'>"
+    for(var j=0;j<Menu_List.length;j++){
+    htmlDATA = htmlDATA+ "<div class='cards'>" + "<img src='images/pizzaImg.png'>" + Menu_List[j] +"</div>";
+    htmlDATA = htmlDATA + "</section>";
+    }
+    document.getElementById("Display_AddedMenu").innerHTML = htmlDATA;
+}
